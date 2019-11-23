99558ee1b1bbf732fd4e1995096dd43c631dc3a4
Sol ji Choi <bssj9307@gmail.com>
2019-11-23 21:14:24 +0900

    accounts urls

--- LAST_PJT/accounts/urls.py
+++ LAST_PJT/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('', views.user_list, name='user_list'),
     path('signup', views.signup, name='signup'),
