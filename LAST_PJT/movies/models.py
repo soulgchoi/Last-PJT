96fa3982debd60d0318ddb9a3f241ad39742976b
choi sol ji <bssj9307@gmail.com>
2019-11-25 14:54:55 +0900

    movies view

--- LAST_PJT/movies/models.py
+++ LAST_PJT/movies/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-class Boxoffice(models.Model):
-    term = models.IntegerField()
-    rank = models.IntegerField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-
-
 class Genre(models.Model):
     name = models.CharField(max_length=30)
 
@@ -17,6 +11,12 @@ class Movie(models.Model):
     poster_url = models.TextField()
     genres = models.ManyToManyField(Genre, related_name='movie_genre')
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
+
+
+class Boxoffice(models.Model):
+    term = models.IntegerField()
+    rank = models.IntegerField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
 
 class Rating(models.Model):
